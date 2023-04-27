476096f1d83e94fff3a15d34c7d63bd5dc273f9e
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-04-27 17:53:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import server from './services/server';
 import logger from './logs/logger';
 import Config from './config';
 
-const PORT = process.env.PUERTO || Config.PUERTO;
+const PORT = process.env.PORT || Config.PORT;
 
 server.listen(PORT, () => logger.info(`Server UP - Escuchando en el puerto ${PORT} - PID WORKER ${process.pid} `));
 
